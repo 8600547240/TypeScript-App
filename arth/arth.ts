70156c0a010543573d1814ebb3370742012bcca7
8600547240 <134043861+8600547240@users.noreply.github.com>
2024-06-18 19:34:33 +0530

    first commit

---
+++ arth/arth.ts
@@ -0,0 +1,14 @@
+class Arth{
+    n1:number=10
+    n2:number=20
+    n3:number=30
+    sum(){
+        console.log(this.n1+this.n2)
+    }
+    sub():number{
+        return this.n1-this.n2
+    }
+}
+var obj=new Arth();
+obj.sum();
+console.log(obj.sub())
